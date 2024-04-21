55be86e8e16518fc585b79a7a10b0da9a850e2bb
Your Username <your.email@example.com>
2024-04-21 18:22:19 +0500

    first commit2

--- zee/components/About.jsx
+++ zee/components/About.jsx
@@ -3,20 +3,22 @@ import React from "react";
 const About = () => {
   return (
     <div id="about" className="p-10 sm:p-0 max-w-[1100px] m-auto">
-      <h1 className="font-bold  text-white  sm:text-7xl text-5xl sm:text-start text-center ">About Me</h1>
-      <p className="text-normal text-white  mt-14 text-xl" >
-        I am Danial Aziz, a seasoned web developer and programmer holding a
-        Bachelor's degree in <span>Software Engineering</span> from{" "}
-        <span>Capital University of Science and Technology</span> . I specialize
-        in architecting streamlined and user-centric web solutions, consistently
-        staying at the forefront of evolving industry paradigms.
+      <h1 className="font-bold  text-white  sm:text-7xl text-5xl sm:text-start text-center ">
+        About Me
+      </h1>
+      <p className="text-normal text-white mt-14 text-xl">
+        Hi, I'm Danial Aziz, the Director at W & B Global, where I oversee web
+        development initiatives. Armed with a Bachelor's degree in Software
+        Engineering from Capital University of Science and Technology, I bring
+        extensive experience as a web developer and programmer.
       </p>
       <p className="text-xl text-white lead-tight mt-5">
-        My commitment to excellence is unequivocal, manifested in my adeptness
-        at seamlessly translating intricate concepts into pragmatic
-        applications. Thriving in collaborative ecosystems, I bring a dynamic
-        skill set to every project. Let us together elevate your digital
-        presence to unprecedented heights.
+        As Director at W & B Global, I orchestrate our web development endeavors
+        while also spearheading strategic initiatives, resource management, and
+        cross-team collaboration. My relentless pursuit of excellence fuels my
+        passion for crafting user-centric web solutions that seamlessly align
+        with our business objectives. Let's collaborate to enhance your digital
+        presence and achieve unprecedented success.
       </p>
     </div>
   );
